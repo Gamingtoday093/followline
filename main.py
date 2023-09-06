28d5ea81ac6130dbfdc8d624fa26443d83e866be
Axel Lankinen Härdling <al225ww@student.lnu.se>
2023-09-06 13:46:02 +0200

    Fixed Intellisense

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
-
 
 # Create your objects here.
 ev3 = EV3Brick()
 
+# Motor definitions
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Sensor definitions
+left_light = ColorSensor(Port.S2)
+right_light = ColorSensor(Port)
 
 # Write your program here.
 ev3.speaker.beep()
