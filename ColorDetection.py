14fed0336fec66253f73d63402a37b9ed34ef6d2
Axel Lankinen Härdling <al225ww@student.lnu.se>
2023-09-06 14:30:52 +0200

    Color Detection Test

---
+++ ColorDetection.py
@@ -0,0 +1,49 @@
+from pybricks.ev3devices import ColorSensor
+
+EQUALS_TOLERENCE = 5
+
+class ColorHDR:
+    def __init__(self, rgb: tuple[int, int, int], reflection: int, ambient: int):
+        self.Color = (rgb[0], rgb[1], rgb[2], reflection, ambient)
+
+    @classmethod
+    def fromColorSensor(cls, sensor: ColorSensor):
+        return cls(sensor.rgb(), sensor.reflection(), sensor.ambient())
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, ColorHDR):
+            return False
+        
+        for c in range(len(self.Color)):
+            offset = self.Color[c] - __value.Color[c]
+            if offset > EQUALS_TOLERENCE or offset < -EQUALS_TOLERENCE:
+                return False
+
+        return True
+    
+    def rgb(self) -> tuple[int, int, int]:
+        """ rgb() -> tuple[int, int, int]
+
+            Returns:
+            Tuple of reflections for red, green, and blue light, each
+            ranging from 0.0% (no reflection) to 100.0% (high reflection).
+        """
+        return (self.Color[0], self.Color[1], self.Color[2])
+    
+    def reflection(self) -> int:
+        """ reflection() -> int %:
+        
+            Returns:
+            Reflection, ranging from 0% (no reflection) to
+            100% (high reflection).
+        """
+        return self.Color[3]
+    
+    def ambient(self) -> int:
+        """ ambient() -> int: %
+
+            Returns:
+            Ambient light intensity, ranging from 0% (dark)
+            to 100% (bright).
+        """
+        return self.Color[4]
--- main.py
+++ main.py
@@ -7,8 +7,23 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+from ColorDetection import ColorHDR
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
+
+col1 = ColorHDR((0, 0, 0), 2, 6)
+col2 = ColorHDR((2, 3, 2), 0, 0)
+
+print(col1 == col2)
+input()
+
+
+def DetectingLineColor(sensor: ColorSensor) -> bool:
+    # Read the color and reflection
+    color = ColorHDR.fromColorSensor(sensor)
+
+    return color.ambient() > 80
 
 # Create your objects here.
 ev3 = EV3Brick()
@@ -20,10 +35,11 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
 # Sensor definitions
 left_light = ColorSensor(Port.S2)
-right_light = ColorSensor(Port)
+right_light = ColorSensor(Port.S1)
 
 # Write your program here.
-ev3.speaker.beep()
+if DetectingLineColor(left_light):
+    ev3.speaker.beep()
 ev3.speaker.beep()
 
 #hejhej
