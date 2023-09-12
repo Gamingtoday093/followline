cb418aa98ae72ee81a51b7cce41f7a1a540ab75b
Axel Lankinen Härdling <al225ww@student.lnu.se>
2023-09-12 14:17:11 +0200

    removed venv

--- ColorDetection.py
+++ ColorDetection.py
@@ -2,6 +2,12 @@ from pybricks.ev3devices import ColorSensor
 
 EQUALS_TOLERENCE = 10
 
+class ColorCompareUsage:
+    def __init__(self, useRGB, useReflection, useAmbient) -> None:
+        self.UseRGB = useRGB
+        self.UseReflection = useReflection
+        self.UseAmbient = useAmbient
+
 class ColorHDR:
     def __init__(self, rgb: tuple[int, int, int], reflection: int, ambient: int):
         self.Color = (rgb[0], rgb[1], rgb[2], reflection, ambient)
@@ -48,3 +54,11 @@ class ColorHDR:
         """
         return self.Color[4]
     
+    @staticmethod
+    def compare(color1, color2) -> (bool, bool, bool):
+        return True, False, False
+
+a = ColorHDR((0, 0, 0), 0, 0)
+b = ColorHDR((0, 0, 0), 0, 0)
+
+print(ColorHDR.compare(a, b))
